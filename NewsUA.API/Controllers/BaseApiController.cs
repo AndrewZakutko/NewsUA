e7730b232388a83b775f026e6c26b9e50c180770
Andrii Zakutko <zakutkoandrey@gmail.com>
2023-05-16 15:36:39 +0200

    Old client project has been deleted

--- NewsUA.API/Controllers/BaseApiController.cs
+++ NewsUA.API/Controllers/BaseApiController.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 
 namespace NewsUA.API.Controllers
 {
     [ApiController]
+    [EnableCors("OpenCORSPolicy")]
     [Route("[controller]")]
     public class BaseApiController : ControllerBase
     {
--- NewsUA.API/Program.cs
+++ NewsUA.API/Program.cs
@@ -2,17 +2,26 @@ using Microsoft.EntityFrameworkCore;
 using NewsUA.API.Data;
 using NewsUA.API.Interfaces;
 using NewsUA.API.Repositories;
-
+ 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlServer(@"Data Source=.\SQL2016;Initial Catalog=NewsDb;Trusted_Connection=True;TrustServerCertificate=True");
 });
+
 builder.Services.AddScoped<INewsRepository, NewsRepository>();
+
+builder.Services.AddCors(opt => opt.AddPolicy("OpenCORSPolicy", builder => {
+    builder.WithOrigins("*")
+        .AllowAnyMethod()
+        .AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
+app.UseCors("OpenCORSPolicy");   
 app.MapControllers();
 
 app.Run();
